be50ca2bbd70be370954db4bedfceda9c85334d2
Øyvind Samuelsen <oyvinsam@stud.ntnu.no>
2020-03-23 19:17:54 +0100

    Init task b.

--- monitorAndDisplay.py
+++ monitorAndDisplay.py
@@ -1,73 +1,47 @@
 from sense_hat import SenseHat
-import threading
-import time
- 
-# to execute sense function
+import json, time
 
-def t1():
-    sense = SenseHat()
 
-# display is 8x8 pixels
+class SenseTemp:
 
-O = [0, 0, 255]
+    # colors
+    R = [255, 0, 0]
+    G = [0, 255, 0]
+    B = [0, 0, 255]
 
-# cold blue
+    # temperature levels
+    cold = 'cold'
+    comfortable = 'comfortable'
+    hot = 'hot'
 
-temperature_cold = [
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O
-]
+    led_displays = {
+        cold: [B for _ in range(64)],
+        comfortable: [G for _ in range(64)],
+        hot: [R for _ in range(64)]
+    }
 
-sense.set_pixels(temperature_cold)
+    def __init__(self, json_file: str):
+        with open(json_file, "r+") as f:
+            self.temps = json.load(f)
+        f.close()
+        self.sense = SenseHat()
 
-O = [0, 255, 0]
+    def get_temp_level(self, temperature: int):
+        if temperature < self.temps['cold_max']:
+            return SenseTemp.cold
+        elif temperature < self.temps['comfortable_max']:
+            return SenseTemp.comfortable
+        else:
+            return SenseTemp.hot
 
-# comfortable green
+    def display_temp(self):
+        temp = self.sense.get_temperature()
+        display_temp = SenseTemp.led_displays[self.get_temp_level(temp)]
+        self.sense.set_pixels(display_temp)
 
-temperature_comfortable = [
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O
-]
 
-sense.set_pixels(temperature_comfortable)
-
-O = [255, 0, 0]
-
-# hot red
-
-temperature_hot = [
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O
-]
-
-sense.set_pixels(temperature_hot)
- 
-# to make it everty 10 seconds
-def t2():
-    while 1:
-        t1()
-        time.sleep(10)
- 
 if __name__ == '__main__':
-    t = threading.Thread(target=t2)
-    t.start()
-
-
+    senseTemp = SenseTemp('config.json')
+    while True:
+        senseTemp.display_temp()
+        time.sleep(10)
