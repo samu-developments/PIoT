49976783950e58f3816c25728d417ccca7662988
Øyvind Samuelsen <oyvinsam@stud.ntnu.no>
2020-03-29 14:44:22 +0200

    add tests for validating config values

--- test/test_monitor_and_display.py
+++ test/test_monitor_and_display.py
@@ -4,33 +4,43 @@ from unittest.mock import Mock
 
 sys.modules['sense_hat'] = Mock()  # mock whole sense-hat module, which is imported below
 
-from monitor_and_display import SenseTemp
+from task_b.monitor_and_display import SenseTemp
+
+
+test_dir = os.path.abspath(os.path.dirname(__file__))
 
 
 class TestMonitor(unittest.TestCase):
 
-    def test_wrong_config(self):
+    def test_validate_config_wrong_config_filename(self):
         try:
-            SenseTemp(Mock(), 'wrong_filename')
+            SenseTemp(Mock(), test_dir + '/wrong_filename')
         except RuntimeError as e:
             self.assertEqual(e.args[0], 'Error, config file not found')
 
+    def test_validate_config_wrong_config_parameters(self):
         try:
-            SenseTemp(Mock(), os.getcwd() + '/config_corrupt.json')
+            SenseTemp(Mock(), test_dir + '/config_corrupt.json')
         except RuntimeError as e:
             self.assertEqual(e.args[0], 'Error, could not read config')
 
         try:
-            SenseTemp(Mock(), os.getcwd() + '/config_wrong_key.json')
-        except RuntimeError as e:
+            SenseTemp(Mock(), test_dir + '/config_wrong_key.json')
+        except ValueError as e:
             self.assertEqual(e.args[0], 'Config values are wrong')
 
         try:
-            SenseTemp(Mock(), 'config_wrong_value.json')
-        except RuntimeError as e:
+            SenseTemp(Mock(), test_dir + '/config_wrong_value_type.json')
+        except ValueError as e:
             self.assertEqual(e.args[0], 'Config values are wrong')
 
         try:
-            SenseTemp(Mock(), os.getcwd() + 'config.json')
+            SenseTemp(Mock(), test_dir + '/config_wrong_value.json')
+        except ValueError as e:
+            self.assertEqual(e.args[0], 'Config values are wrong')
+
+    def test_validate_config_valid_config(self):
+        try:
+            SenseTemp(Mock(), test_dir + '/config.json')
         except Exception:
             self.fail("Failed initializing valid config.json")
